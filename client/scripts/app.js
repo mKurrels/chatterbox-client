f086981a40076066c3694a20649a3f706f590f91
mKurrels <mjkurrels@gmail.com>
2015-07-13 19:34:57 -0700

    done with basic stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,20 +2,52 @@
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
   friends: {},
-  init: function(){},
+  messages: undefined,
+  rooms: {},
+  init: function(){
+    app.clearMessages();
+    for (var i = 0; i <app.messages.length; i++){
+      //if messages.room isnt in options list
+      //if($("#roomSelect option:selected" ).text() === messages[i].roomname)
+        //add room to options
+      var roomname = unescapeHtml(escapeHtml(app.messages[i].roomname));
+      if(!app.rooms.hasOwnProperty(roomname)){
+        app.rooms[roomname] = roomname;
+        var $newOption = $('<option></option>').text(roomname);
+        $("#roomSelect").append($newOption);
+      }
+      //if message is in currently selected room
+      if($("#roomSelect option:selected" ).text() === roomname){
+        app.addMessage(app.messages[i])
+      }
+    }
+  },
   send: function(message){
     $.ajax({
       url: app.server, 
       type: 'POST',
-      data: JSON.stringify(message)      
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+          console.log('chatterbox: Message sent');
+          app.init();
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }      
     });
   },
   fetch: function(){
-    return $.ajax({
+    console.log('hi');
+    $.ajax({
       url: app.server, 
       type: 'GET',
+      contentType: 'application/json',
       success: function(data){
-        console.log(data)
+        console.log('bob')
+        app.messages = data.results;
+        app.init();
       }  
     });
   },
@@ -24,11 +56,10 @@ var app = {
   },
   addMessage: function(message){
     var $message = $('<div></div>');
-    var username = message.username;
-    var text = message.text;
-    var roomname = message.roomname;
-    var $username = $('<h3 class="username"></h3>').text(username);
-    var $text = $('<p></p>').text(text);
+    var username = escapeHtml(message.username);
+    var text = escapeHtml(message.text);
+    var $username = $('<h3 class="username"></h3>').text(unescapeHtml(username));
+    var $text = $('<p></p>').text(unescapeHtml(text));
     $username.appendTo($message);
     $text.appendTo($message);
     $('#chats').append($message);
@@ -42,14 +73,37 @@ var app = {
     console.log(username);
   },
   handleSubmit: function(){
-    console.log('Hello I have submitted yo');
+    var username = window.location.search.split('=')[1];
+    var message = {
+      username: username,
+      text: $('#message').val(),
+      roomname: $("#roomSelect option:selected" ).text()
+    };
+    app.send(message);
   }
 };
 
 $('document').ready(function(){
+  app.fetch();
   $('#main').on('click', '.username', function(){app.addFriend($(this).text())});
   $('#send').submit(function(){app.handleSubmit()});
+  $('#roomSelect').change(app.init);
+  $('#refresh').on('click', function(){app.fetch();});
+
 });
+
+var escapeHtml = function(str){
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+var unescapeHtml = function(escapedStr){
+  var div = document.createElement('div');
+  div.innerHTML = escapedStr;
+  var child = div.childNodes[0];
+  return child ? child.nodeValue : '';
+}
 
 // $.ajax({
 //   // This is the url you should use to communicate with the parse API server.
